32afd05b130109d0f1454d782269728032d51091
manveechouhan2007-rgb <manveechouhan2007@gmail.com>
2026-08-31 11:32:18 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3); // not a good way as code readability should be high
+
+console.log("manvee"); //good way
+
+let name = "Manvee"
+let age = 18
+let isLoggedIn = false
+let State;
+
+console.log(typeof name);
