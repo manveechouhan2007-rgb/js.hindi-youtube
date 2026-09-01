73038d0a1d1d6e1503f3903e9bde2f2795d7e574
manveechouhan2007-rgb <manveechouhan2007@gmail.com>
2026-09-01 13:12:56 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,39 @@ console.log(valueInNumber)
 let isLoggedIn="1"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+
+// ***********************operations**************************
+
+let value=3
+let negValu = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1="hello"
+let str2=" world"
+let str3= str1 + str2
+console.log(str3);
+
+console.log("1"+2); //12  agr starting m string h to vo pure ko string m hi consider karegi wrna bd m h to pehle wale operations as pr maths honge 
+console.log(1+"2");  //12
+console.log("1"+2+2);  //122
+console.log(1+2+"2");  //32  
+
+console.log(+true);  // 1
+console.log(+"");   //0
+
+let num1,num2,num3
+num1=num2=num3= 2+2  //not a good way as code readability is less although you'll try to look smart but not so cool ans prefferable method
+
+let gameCounter=100  //101
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter=100   //101
+++gameCounter;
+console.log(gameCounter);
